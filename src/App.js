4bdb3cc494c7f4d8a8002cd5068f2cf9f3583946
Maryna Zhelobetska <marynazhelobetska@gmail.com>
2023-12-31 13:54:26 +0200

    React_app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import NewsItem from "./components/NewsItem";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const newsItem = {
+    title: 'Some news',
+    body: 'About this news',
+  };
+  return <NewsItem news = {newsItem}/>;
 }
 
 export default App;
+
+
+
+
